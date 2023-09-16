c74c9ead260fe50cae74359b007329cdf7864d28
Kinka <katyamew@gmail.com>
2023-09-16 19:50:05 +1000

    Звуки и шрифт

---
+++ Assets/Script/GameManagerScript.cs
@@ -0,0 +1,114 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    //Костыльно так нельзя нужен scrObj
+    [SerializeField] GameObject MenuUI;
+    [SerializeField] GameObject GameUI;
+    [SerializeField] GameObject SettingsUI;
+
+    [SerializeField] AudioSource menuSoundPlayer;
+    [SerializeField] AudioSource gameSoundPlayer;
+    
+
+    private GameObject currentActiveUI;
+    private bool inGame = false;
+
+    private void Start()
+    {
+       
+        AudioListener.volume = PlayerPrefs.GetFloat("volume");
+        this.currentActiveUI = MenuUI;
+        menuSoundPlayer.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && inGame)
+        {
+            this.openMenu();
+            gameSoundPlayer.Pause();
+            menuSoundPlayer.Play();
+        }
+    }
+
+
+    //Закрытие приложения игры
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+    
+
+    //Открытие настроек игры
+    public void openSettings()
+    {
+        this.swapVisibleUi(currentActiveUI, SettingsUI);
+        GameObject.FindGameObjectWithTag("SoundSettingsSlider").GetComponent<Slider>().value = PlayerPrefs.GetFloat("volume");
+    }
+
+    public void openMenu()
+    {
+        pauseGame();
+        this.swapVisibleUi(currentActiveUI, MenuUI);
+        this.inGame = false;
+    }
+
+    //Переход к игре
+    public void backToGame()
+    {
+        resumeGame();
+        this.swapVisibleUi(currentActiveUI, GameUI);
+        menuSoundPlayer.Pause();
+        gameSoundPlayer.Play();
+        this.inGame = true;
+    }
+
+    //Сохранение игрововго прогресса
+    public void saveGame()
+    {
+        //Тут нужно найти менеджер игровых уровней и получить от него массив собранных кветов и их тип и  сохранить в строку
+        string saveString = "";
+        PlayerPrefs.SetString("save", saveString);
+    }
+
+    public void loadGame()
+    {
+
+        string data = PlayerPrefs.GetString("save");
+        //Тут нужно эту структуру проинициализировать и выполнить действия для загрузки юзера на уровень
+
+        backToGame();
+    }
+
+    //Пауза
+    public void pauseGame()
+    {
+       Time.timeScale = 0f;
+    }
+
+    //Возобновление игрового процесса
+    public void resumeGame()
+    {
+        Time.timeScale = 1f;
+    }
+
+    //Меняет активынй UI
+    private void swapVisibleUi(GameObject hide, GameObject show)
+    {
+        hide.SetActive(false);
+        show.SetActive(true);
+        currentActiveUI = show;
+    }
+
+    //Изменеие громкости звука
+    public void changeGameVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("volume", AudioListener.volume);
+    }
+
+}
