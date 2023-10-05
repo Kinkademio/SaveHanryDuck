344b6509a8bb37090aa32a44b42f175be3335734
Kinkademio <60955217+Kinkademio@users.noreply.github.com>
2023-10-05 13:31:37 +1000

    *

--- Assets/Script/Score/ScoreController.cs
+++ Assets/Script/Score/ScoreController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using UnityEditor.U2D.Path.GUIFramework;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
